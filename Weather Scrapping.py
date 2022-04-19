b2256487f97e4d2c736cc2531809e32e33b49a54
GuillermoPomphile <82566419+GuillermoPomphile@users.noreply.github.com>
2022-04-19 11:21:11 +0200

    first upload

---
+++ Weather Scrapping.py
@@ -0,0 +1,17 @@
+import json
+import urllib.request
+
+# API from Openweather.com
+key = "58f2c87f1403e04bd64695326a85f33a"
+
+#Coordinates of Bern, Bolligenstrasse 14
+latitude = 46.953819
+longitude = 7.465260
+
+language = "de"  # de for German, en for English
+
+response = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={key}@lang{language}')
+
+j = json.load(response)
+
+print(j["weather"]["description"])
